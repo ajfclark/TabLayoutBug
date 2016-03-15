5728ac6b725603758d32ae0bf6e0e39ff562999b
Andrew Clark <ajfclark@gmail.com>
2016-03-15 14:01:13 +1100

    23.2.1 Tab isn't selected after creation

--- app/src/main/java/com/example/tablayoutselectbug/MainActivity.java
+++ app/src/main/java/com/example/tablayoutselectbug/MainActivity.java
@@ -13,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
 
         for (int i = 0; i < 3; i++) {
-            // This should no crash, but does under 23.1.1
-            // tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_layout));
+            // This crashed in 23.1.1
+            //tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_layout));
 
             TabLayout.Tab tab = tabLayout.newTab();
+
+            // If tab is added like this, the first tab is selected by default
+            //tab.setCustomView(R.layout.tab_layout);
+            //tabLayout.addTab(tab);
+
+            // If the tab is added like this, the selection doesn't work
             tabLayout.addTab(tab);
             tab.setCustomView(R.layout.tab_layout);
         }
